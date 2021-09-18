e589dfecdb4bfda76449acc057c47ede75d39e82
hrudaya <32632363+hrudaya@users.noreply.github.com>
2021-09-18 15:46:49 +0530

    Update main.tf

--- Challenge1/modules/alb/main.tf
+++ Challenge1/modules/alb/main.tf
@@ -1,4 +1,3 @@
-# Get the ubuntu ami id
 data "aws_ami" "ubuntu" {
   most_recent = true
   filter {
@@ -8,18 +7,18 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"]
 }
 
-#Create AWS instance with ami id and instance type
-resource "aws_instance" "webserver" {
-  name_prefix     = var.namespace
-  image_id        = data.aws_ami.ubuntu.id
-  instance_type   = "t2.micro"
-  key_name        = var.ssh_keypair
-  
+resource "aws_launch_template" "webserver" {
+  name_prefix   = var.namespace
+  image_id      = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  key_name      = var.ssh_keypair
+  iam_instance_profile {
+    name = module.iam_instance_profile.name
+  }
   vpc_security_group_ids = [var.sg.webserver]
 }
 
-# Create a Load Balancer Group
-resource "aws_alb_group" "webserver" {
+resource "aws_autoscaling_group" "webserver" {
   name                = "${var.namespace}-asg"
   min_size            = 1
   max_size            = 3
@@ -31,8 +30,7 @@ resource "aws_alb_group" "webserver" {
   }
 }
 
-# Create a Load balancer module  
-module "alb"{
+module "alb" {
   source             = "terraform-aws-modules/alb/aws"
   version            = "~> 5.0"
   name               = var.namespace
